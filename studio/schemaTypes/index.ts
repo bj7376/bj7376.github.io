085e63ada252c29cb9e7406917319b6707dd3eb9
bj7376 <109680522+bj7376@users.noreply.github.com>
2026-09-14 22:41:28 +0900

    Register portfolio schemas

--- studio/schemaTypes/index.ts
+++ studio/schemaTypes/index.ts
@@ -1,1 +1,21 @@
-export const schemaTypes = []
+import {award} from './documents/award'
+import {bio} from './documents/bio'
+import {exhibition} from './documents/exhibition'
+import {journalPost} from './documents/journal-post'
+import {project} from './documents/project'
+import {publication} from './documents/publication'
+import {siteSettings} from './documents/site-settings'
+import {projectBody} from './objects/project-body'
+import {richText} from './objects/rich-text'
+
+export const schemaTypes = [
+  richText,
+  projectBody,
+  bio,
+  project,
+  publication,
+  award,
+  exhibition,
+  siteSettings,
+  journalPost,
+]
