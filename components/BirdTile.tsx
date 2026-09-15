7216dd827df147b1e07c01b20198009eacf9a019
bj7376 <109680522+bj7376@users.noreply.github.com>
2026-09-15 13:29:38 +0900

    Use eBird slug in bird tiles

--- components/BirdTile.tsx
+++ components/BirdTile.tsx
@@ -4,7 +4,7 @@ import { BirdName } from "@/components/BirdName";
 
 export function BirdTile({ bird }: { bird: Species }) {
   return (
-    <Link className="bird-tile" href={`/birding/species/${bird.code}`}>
+    <Link className="bird-tile" href={`/birding/species/${bird.slug}`}>
       <div className="bird-photo-placeholder"><span>{bird.commonName.slice(0, 1)}</span></div>
       <div><BirdName bird={bird} /><span>{bird.scientificName}</span></div>
     </Link>
